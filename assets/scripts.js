871e98ff075e83a8595f7500d01201cf0781e470
justine frison <frisonjustine0@gmail.com>
2025-12-05 13:39:01 +0100

    mimification de css

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,19 +3,4 @@
 // pour afficher les images en grand, et des filtres (tags) affichés en haut.
 
 
-
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
-    });
-});
+$(document).ready(function(){$('.gallery').mauGallery({columns:{xs:1,sm:2,md:3,lg:3,xl:3},lightBox:true,lightboxId:'myAwesomeLightbox',showTags:true,tagsPosition:'top'});});
